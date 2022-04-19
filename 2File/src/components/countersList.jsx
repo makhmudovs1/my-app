ed2a2beee6f49e72fb4d97d4a619f9c2378ad35f
sherafgan <sherafgan.makhmudov@mail.ru>
2022-04-19 17:40:44 +0300

    add

---
+++ 2File/src/components/countersList.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react"
+import Counter from "./counter";
+
+const CountersList = () => {
+    const initialState = [
+        {id: 0, value: 0, name: 'Ненужная вещь'},
+        {id: 1, value: 4, name: 'Ложка'},
+        {id: 2, value: 0, name: 'Вилка'},
+        {id: 3, value: 0, name: 'Тарелка'},
+        {id: 4, value: 0, name: 'Набор минималиста'},
+    ]
+
+    const [counters, setCounters] = useState(initialState);
+
+    const handleDelete = (id) => {
+        const newCounters = counters.filter(c => c.id !== id);
+        setCounters(newCounters);
+    }
+
+    const handleReset = () => {
+        setCounters(initialState);
+    }
+
+    const handleIncrement = (id) => {
+        setCounters(counters.map(c => c.id === id ? {...c, value: c.value + 1} : {...c}));
+    }
+
+    const handleDecrement = (id) => {
+        setCounters(counters.map(c => c.id === id ? {...c, value: c.value - 1} : {...c}));
+    }
+    return (
+        <>
+            {counters.map(count => <Counter
+                key={count.id}
+                {...count}
+                onDelete={handleDelete}
+                onIncrement={handleIncrement}
+                onDecrement={handleDecrement}
+            />)}
+            <button className='btn btn-primary btn-sm m-2' onClick={handleReset}>reset</button>
+        </>
+    )
+}
+
+export default CountersList;
